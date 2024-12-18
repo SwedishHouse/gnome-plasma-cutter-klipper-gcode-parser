81630036c51dd676d8b4f0dea1d9b36803c0653b
Вадим Мещеряков <swedish2015@yandex.ru>
2024-12-18 13:45:24 +0400

    result - it's broken macro

--- gcode_parser/multiple_gcmd.py
+++ gcode_parser/multiple_gcmd.py
@@ -1,31 +1,14 @@
 import re
 
 
-# class Gcode:
-#
-#     def __init__(self, name: str, params: dict[str, float] | None) -> None:
-#         self.name = name
-#         self.params = params
-#
-#     @property
-#     def get_param(self) -> str:
-#         if self.params:
-#             return ' '.join(key + str(val) for key, val in self.params.items())
-#         return ''
-#
-#     @property
-#     def get_name(self) -> str:
-#         return self.name
-#
-#     def get_cmd(self):
-#         return self.name + ' ' * bool(self.get_param) + self.get_param
-
 class GCode:
+
     PATTERN = r"([A-Z]+)([-+]?\d*\.?\d+)"
+
     def __init__(self, line: str) -> None:
-        gcode = line.split()
-        self.cmd = gcode[0]
-        self.params = self.convert_to_dict(gcode[1::])
+        cmd = line.split()
+        self.cmd = cmd[0]
+        self.params = self.convert_to_dict(cmd[1::])
 
     def get(self):
         return {"cmd": self.cmd, "params": self.params}
@@ -57,10 +40,17 @@ class GCode:
     def get_name(self) -> str:
         return self.cmd
 
+
 class GCodeSplitter:
 
     def __init__(self) -> None:
-        self.PATTERN = r'([GMSFTDH]-?\d+\.?\d*)|([XYZIJKR]-?\d+\.?\d*)|\(.*?\)'
+        self.PATTERN = r'([GMSFTDH]-?\d+\.?\d*)|([XYZABCIJKR]-?\d+\.?\d*)|\(.*?\)'
+
+    @staticmethod
+    def is_coordinate_command(command):
+        # Регулярное выражение для проверки, содержит ли команда только координаты
+        pattern = r"\b(?:[XYZABC](?:\+|-)?\d+(?:\.\d+)?)|[IJKP](?:\+|-)?\d+(?:\.\d+)?\b"
+        return re.match(pattern, command) is not None
 
     def parse_gcode_line(self, line: list) -> list:
         # Регулярное выражение для поиска команд и их параметров
@@ -74,7 +64,8 @@ class GCodeSplitter:
 
             for match in matches:
                 command = match[0] if match[0] else match[1]
-
+                if re.match(r'^[GMS][0-9]+(\.[0-9]+)?$', command):
+                    command = command.replace('.', '_')
                 # Если это новая команда, добавляем ее в список
                 if re.match(r'[GMSFTDH]', command):
                     if current_command:
@@ -96,5 +87,6 @@ class GCodeSplitter:
 if __name__ == '__main__':
     gcode = GCode("G00 X15.2")
     splitter = GCodeSplitter()
-    data = "G91\nG01 X100 Y-152.2 Z21.1\nG90".split('\n')
+    data = "QUERY_ENDSTOP".split('\n')
     res = splitter.parse_gcode_line(data)
+    # print(splitter.is_coordinate_command("X-100 Y+152.2 Z32"))
